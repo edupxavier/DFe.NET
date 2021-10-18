cf1060168bc81281217d9ec6f8a05342e820ee00
edupxavier <49204455+edupxavier@users.noreply.github.com>
2021-10-18 19:07:07 -0300

    fazendo o parse no xml

---
+++ NFe.Mini.AppTeste.NetCore/NFe.Mini.AppTeste.NetCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using NFe.Mini.Standard;
+using System;
+using System.IO;
+
+namespace NFe.Mini.AppTeste.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Temp\xml\Autorizada\Transportadas";
+            var arqs = Directory.GetFiles(dir, "*.xml");
+
+            Console.WriteLine($"Processando {arqs.Length} no diretório {dir}...");
+
+            int ok = 0, erro = 0;
+            foreach (var arq in arqs)
+            {
+                try
+                {
+                    string xml = File.ReadAllText(arq);
+                    Console.WriteLine($" - {Path.GetFileName(arq)}");
+                    ok++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($" - {Path.GetFileName(arq)}: ERRO");
+                    Console.WriteLine($"   {ex.GetType().Name}: {ex.Message}");
+                    erro++;
+                }
+            }
+            Console.WriteLine($"\n\n{arqs} arquivos, {ok} ok, {erro} com erro");
+        }
+    }
+}
---
+++ NFe.Mini.Standard/NFe.Mini.Standard/MiniExtensions.cs
@@ -0,0 +1,28 @@
+﻿using NFe.Classes;
+using NFe.Utils.NFe;
+using System;
+using System.IO;
+
+namespace NFe.Mini.Standard
+{
+    public static class MiniExts
+    {
+        public static Classes.NFe CarregaNFe(string xml)
+        {
+            return new Classes.NFe().CarregarDeXmlString(xml);
+
+
+            //nfeProc proc = null;
+            //try
+            //{
+            //    nfeProc = new nfeProc().CarregarDeXmlString(xml);
+                
+            //}
+            //catch //Carregar NFe ainda não transmitida à sefaz, como uma pré-visualização.
+            //{
+            //    //proc = new nfeProc() { NFe = new Classes.NFe().CarregarDeXmlString(xml), protNFe = new Classes.Protocolo.protNFe() };
+            //    //return proc.NFe;
+            //}
+        }
+    }
+}
